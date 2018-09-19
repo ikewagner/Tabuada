fd35a09bc53468ed8cf42fff1cc83a4a72e90cef
ikewagner <wagner.ike@gmail.com>
2018-09-18 22:35:50 -0300

    Add files via upload

---
+++ tabuada.py
@@ -0,0 +1,19 @@
+
+n = int(input('Digite um número para ver a sua tabuada: '))
+t1 = n*1
+t2 = n*2
+t3 = n*3
+t4 = n*4
+t5 = n*5
+t6 = n*6
+t7 = n*7
+t8 = n*8
+t9 = n*9
+t10 = n*10
+print('{}x{}= {} \n{}x{}= {} \n{}x{}= {} \n{}x{}= {} \n{}x{}= {} \n{}x{}= {} \n{}x{}= {} \n{}x{}= {} \n{}x{}= {} \n{}x{}= {}'.format(n,1,t1,n,2,t2,n,3,t3,n,4,t4,n,5,t5,n,6,t6,n,7,t7,n,8,t8,n,9,t9,n,10,t10))
+
+print()
+
+print()
+
+input(' Enter para sair ! ')
